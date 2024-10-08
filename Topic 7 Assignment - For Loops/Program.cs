bbecdb99a7ec466f606f028e479db4cf8496a8d1
Sam Ahrens <339729139@GoTVDSB.ca>
2024-10-08 09:18:26 -0400

    Add project files.

---
+++ Topic 7 Assignment - For Loops/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Topic_7_Assignment___For_Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            int y;
+            for (int i = 10; i >= 1; i = i - 1)
+                Console.Write(i + ".\t");
+            Console.WriteLine();
+            Console.WriteLine();
+            for (int i = -10; i <= 10; i = i + 2)
+            {
+                y = i * i;
+                Console.WriteLine(i + "\t" + y);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("What is your name?");
+            name = Console.ReadLine().ToUpper();
+            Console.WriteLine();
+
+            if (name == "ALDWORTH")
+                for (int i = 1; i <= 5; i = i + 1)
+                    Console.WriteLine(name);
+            else
+            {
+                for (int i = 1; i <= 10; i = i + 1) 
+                    Console.WriteLine(name);
+            }
+        }
+    }
+}
